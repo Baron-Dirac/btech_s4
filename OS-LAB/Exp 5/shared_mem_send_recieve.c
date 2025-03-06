170b4b3b8dee9d533e83b3dc91494d10cb4ab518
Baron-Dirac <137907278+Baron-Dirac@users.noreply.github.com>
2025-03-06 08:22:28 +0530

    Create shared_mem_send_recieve.c
    Experiment 5 part A

---
+++ OS-LAB/Exp 5/shared_mem_send_recieve.c
@@ -0,0 +1,21 @@
+/*PROGRAM TO IMPLEMENT SHARED MEMORY:RECEIVER&SENDER*/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<string.h>
+void main()
+{
+int shmid,p,f=1;
+int *shared_memory;
+char buffer[100];
+shmid=shmget(1234,1024,0666);
+printf("THE KEY OBTAINED :%d\n\n",shmid);
+shared_memory=shmat(shmid,NULL,0);
+printf("THE PROCESS ATTACHED AT :%p\n",shared_memory);
+for(int i=1;i<=*shared_memory;i++)
+{
+f=f*i;
+}
+printf("The factorial is:%d\n",f);
+}
