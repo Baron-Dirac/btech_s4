c549ebc922f5672404724c811fc4436e17232f61
Baron-Dirac <137907278+Baron-Dirac@users.noreply.github.com>
2025-03-10 14:36:53 +0530

    Create testbench.sv

---
+++ Digital LAB/OR Gate/testbench.sv
@@ -0,0 +1,25 @@
+// Code your testbench here
+// or browse Examples
+module OR_Gate_tb;
+  reg A;
+  reg B;
+  wire Y;
+  integer i;
+  
+  OR_Gate inst(.A(A), .B(B), .Y(Y));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    #100 $finish;
+  end
+  
+  initial 
+    begin
+      A = 0; B = 0;
+      #5A = 0; B = 0;
+      #5A = 1; B=0;
+      #5A = 1; B=1;
+    end
+   
+endmodule
